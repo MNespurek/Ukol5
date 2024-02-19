cd7b072361b9a612fd81dd8f19e67c9549f4b8d7
michal.nespurek <michal.nespurek@email.cz>
2024-02-19 16:59:43 +0100

    Třída nastavení
    definuje oddělovač v textu, soubory, který se má nahrávat včetně metod pro volání těchto hodnot

---
+++ src/main/java/com/engeto/plantProject/Settings.java
@@ -0,0 +1,15 @@
+package com.engeto.plantProject;
+
+public class Settings {
+    private static String FILENAME = "kvetiny.txt";
+
+    private static String DELIMITER = ";";
+
+    public static String getFILENAME() {
+        return FILENAME;
+    }
+
+    public static String getDELIMITER() {
+        return DELIMITER;
+    }
+}
