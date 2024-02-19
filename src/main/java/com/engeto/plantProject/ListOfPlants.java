b2e4f7c914140251d00de62a2de42750bb0d647c
michal.nespurek <michal.nespurek@email.cz>
2024-02-19 13:47:44 +0100

    Vytvoření metod
    nahrávání květin ze souboru
    ukládání seznamu květin do souboru

--- src/main/java/com/engeto/plantProject/ListOfPlants.java
+++ src/main/java/com/engeto/plantProject/ListOfPlants.java
@@ -1,7 +1,10 @@
 package com.engeto.plantProject;
 
+import java.io.*;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class ListOfPlants {
     private List<Plant> plantList;
@@ -28,4 +31,44 @@ public class ListOfPlants {
     public void setPlantList(List<Plant> plantList) {
         this.plantList = new ArrayList<>(plantList);
     }
+
+    public void loadListOfPlants(String fileName) throws PlantException {
+        int lineCounter = 0;
+        plantList.clear();
+        try(Scanner scanner = new Scanner(new BufferedReader(new FileReader(fileName)))) {
+            while(scanner.hasNextLine()) {
+                lineCounter++;
+                String plantLine = scanner.nextLine();
+                String[] splitPlantLine = plantLine.split(Settings.getDELIMITER());
+                String name = splitPlantLine[0];
+                String note = splitPlantLine[1];
+                LocalDate planted = LocalDate.parse(splitPlantLine[2]);
+                LocalDate watering = LocalDate.parse(splitPlantLine[3]);
+                int frequencyOfWatering = Integer.parseInt(splitPlantLine[4]);
+                Plant plant = new Plant(name, note, planted, watering, frequencyOfWatering);
+                plantList.add(plant);
+            }
+        } catch (FileNotFoundException e) {
+            throw new PlantException("Soubor "+fileName+ "nebyl nalezen" +e.getLocalizedMessage()+ ".");
+        }
+    }
+    public void saveListOfPlants(String fileName) throws IOException, PlantException {
+        String definitionDelimiter = Settings.getDELIMITER();
+        try(PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(fileName)))) {
+            for(Plant plant : plantList) {
+                writer.println(plant.getName() + definitionDelimiter
+                        + plant.getNotes() + definitionDelimiter
+                        + plant.getPlanted() + definitionDelimiter
+                        + plant.getWatering() + definitionDelimiter
+                        + plant.getFrequencyOfWatering() + definitionDelimiter);
+            }
+        }
+        catch (FileNotFoundException e) {
+            throw new PlantException("Soubor "+fileName+ "nebyl nalezen" +e.getLocalizedMessage()+ ".");
+        }
+        catch (IOException e) {
+            throw new PlantException("Chyba při načítání souboru "+fileName+"\n"+e.getLocalizedMessage()+".");
+        }
+    }
 }
+
