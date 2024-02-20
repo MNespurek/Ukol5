393d8622ab4ce6d880a03a4aba2b3323f752cea4
michal.nespurek <michal.nespurek@email.cz>
2024-02-20 15:23:58 +0100

    Úprava delimiter na tabulátor

--- src/main/java/com/engeto/plantProject/Settings.java
+++ src/main/java/com/engeto/plantProject/Settings.java
@@ -3,7 +3,7 @@ package com.engeto.plantProject;
 public class Settings {
     private static String FILENAME = "kvetiny.txt";
 
-    private static String DELIMITER = ";";
+    private static String DELIMITER = "\t";
 
     public static String getFILENAME() {
         return FILENAME;
